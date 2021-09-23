c0e9670c28c243e70ccea99a43ffe10f963b061d
Matthew O'Keefe <matt@okeefe.dev>
2021-09-23 06:11:15 -0500

    Reverting index file changes

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(robot, scripts) {
-	'use strict';
-	const scriptsPath = path.resolve(__dirname, 'src');
-	try {
-		const scripts = fs.readdirSync(scriptsPath);
-		let results = [];
-		for (const script in scripts) {
-			if (script && script.indexOf('*') < 0) {
-				if (scripts.indexOf(script) > -1) {
-					results.push(robot.loadFile(scriptsPath, script));
-				}
-			} else {
-				results.push(robot.loadFile(scriptsPath, script));
-			}
-		}
-		return results;
-	} catch (e) {
-		console.log('An error occurred');
-	}
+  'use strict';
+  const scriptsPath = path.resolve(__dirname, 'src');
+  return fs.exists(scriptsPath, function(exists) {
+    if (exists) {
+      const ref = fs.readdirSync(scriptsPath);
+      const results = [];
+      for (const i = 0, len = ref.length; i < len; i++) {
+        const script = ref[i];
+        if (scripts && scripts.indexOf('*') < 0) {
+          if (scripts.indexOf(script) > -1) {
+            results.push(robot.loadFile(scriptsPath, script));
+          }
+        } else {
+          results.push(robot.loadFile(scriptsPath, script));
+        }
+      }
+      return results;
+    }
+  });
 };
