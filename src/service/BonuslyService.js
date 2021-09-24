bee628bd262dadf2ef8efdd4cbc8281b9d6d26af
Matthew O'Keefe <matt@okeefe.dev>
2021-09-24 12:33:57 -0500

    Bumping the version and adding logging to the bonusly service

--- src/service/BonuslyService.js
+++ src/service/BonuslyService.js
@@ -7,7 +7,10 @@ class BonuslyService {
     this.url = procVars.bonuslyUri;
     this.axios = Axios.create({
       baseURL: procVars.bonuslyUri,
-      headers: { Authorization: `Bearer ${procVars.bonuslyApiKey}` },
+      headers: {
+        Authorization: `Bearer ${procVars.bonuslyApiKey}`,
+        'Content-Type': 'application/json',
+      },
     });
   }
 
@@ -17,19 +20,25 @@ class BonuslyService {
    * @returns the user from the scores db, undefined if not found
    */
   async sendBonus(event) {
+    this.robot.logger.debug(`Sending a bonusly bonus to ${JSON.stringify(event.recipient)} from ${JSON.stringify(event.sender)}`);
     let reason = `point given through ${this.robot.name}`;
     if (event.reason) {
       const buff = new Buffer.from(event.reason, 'base64');
       reason = buff.toString('UTF-8');
     }
 
-    const resp = await this.axios.post('/api/v1/bonuses', {
-      giver_email: event.sender.slackEmail,
-      receiver_email: event.recipient.slackEmail,
-      amount: event.amount,
-      hashtag: '#excellence',
-      reason,
-    });
+    let resp;
+    try {
+      resp = await this.axios.post('/api/v1/bonuses', {
+        giver_email: event.sender.slackEmail,
+        receiver_email: event.recipient.slackEmail,
+        amount: event.amount,
+        hashtag: '#excellence',
+        reason,
+      });
+    } catch (e) {
+      this.robot.logger.error('Error sending bonusly bonus', e);
+    }
 
     return resp;
   }
